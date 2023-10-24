eafae25634b419673b98c2c5755e4b8cfa2582d9
Plovester <lovester.solo@gmail.com>
2023-10-24 14:48:03 +0100

    Create a header

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__)
-app.config['UNSPLASH_ACCESS_KEY'] = os.environ.get('UNSPLASH_ACCESS_KEY')
-app.config['UNSPLASH_SECRET_KEY'] = os.environ.get('UNSPLASH_SECRET_KEY')
 Bootstrap5(app)
 
 
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
