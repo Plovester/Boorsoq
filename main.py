7afc08667aa3a040a9e5cfa9d8125e1741a204e8
Plovester <lovester.solo@gmail.com>
2023-10-26 11:32:24 +0100

    Make a main page design

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import os
 from flask import Flask, render_template, redirect, url_for, request
+# from flask_login import login_user, LoginManager, current_user, logout_user, login_required
 from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__)
 Bootstrap5(app)
+
+# login_manager = LoginManager()
+# login_manager.init_app(app)
 
 
 @app.route('/')
