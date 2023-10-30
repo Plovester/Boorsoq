064f271e09d9eae4413b56be1d3f68c5f618368a
Plovester <lovester.solo@gmail.com>
2023-10-30 18:44:41 +0000

    Create a logout functional

--- main.py
+++ main.py
@@ -36,64 +36,73 @@ def load_user(user_id):
     return db.get_or_404(User, user_id)
 
 
-@app.route('/')
-def home():
-    return render_template("index.html")
-
-
 @app.route('/register', methods=["GET", "POST"])
 def register():
     register_form = RegisterForm()
 
-    if request.method == 'POST':
-        user = User.query.filter_by(email=register_form.email.data).first()
+    if not current_user.is_authenticated:
+        if request.method == 'POST':
+            user = User.query.filter_by(email=register_form.email.data).first()
 
-        if not user:
-            if register_form.validate_on_submit():
-                hashed_password = generate_password_hash(register_form.password.data, method='pbkdf2:sha256', salt_length=8)
+            if not user:
+                if register_form.validate_on_submit():
+                    hashed_password = generate_password_hash(register_form.password.data, method='pbkdf2:sha256', salt_length=8)
 
-                new_user = User(
-                    name=register_form.name.data,
-                    email=register_form.email.data,
-                    password=hashed_password
-                )
+                    new_user = User(
+                        name=register_form.name.data,
+                        email=register_form.email.data,
+                        password=hashed_password
+                    )
 
-                db.session.add(new_user)
-                db.session.commit()
+                    db.session.add(new_user)
+                    db.session.commit()
 
-                login_user(new_user)
+                    login_user(new_user)
 
-                return redirect(url_for('home'))
-        else:
-            flash('The email has already exist. Try to log in')
-            return redirect(url_for('login'))
-    return render_template("register.html", form=register_form)
+                    return redirect(url_for('home'))
+            else:
+                flash('The email has already exist. Try to log in')
+                return redirect(url_for('login'))
+        return render_template("register.html", form=register_form)
 
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
 
-    if request.method == 'POST':
-        if login_form.validate_on_submit():
-            user = User.query.filter_by(email=request.form.get('email')).first()
+    if not current_user.is_authenticated:
+        if request.method == 'POST':
+            if login_form.validate_on_submit():
+                user = User.query.filter_by(email=request.form.get('email')).first()
 
-            if user:
-                user_password = user.password
-                password = login_form.password.data
-                is_password_correct = check_password_hash(user_password, password)
+                if user:
+                    user_password = user.password
+                    password = login_form.password.data
+                    is_password_correct = check_password_hash(user_password, password)
 
-                if is_password_correct:
-                    login_user(user)
-                    return redirect(url_for('home'))
+                    if is_password_correct:
+                        login_user(user)
+                        return redirect(url_for('home'))
+                    else:
+                        flash('Your password is not correct. Try again')
+                        return redirect(url_for('login'))
                 else:
-                    flash('Your password is not correct. Try again')
-                    return redirect(url_for('login'))
-            else:
-                flash('The email does not exist. Try again or register')
-                return redirect('login')
+                    flash('The email does not exist. Try again or register')
+                    return redirect('login')
 
     return render_template("register.html", form=login_form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
