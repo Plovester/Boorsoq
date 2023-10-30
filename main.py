6467ed96d79a181870166c4247527dcf51c9bc40
Plovester <lovester.solo@gmail.com>
2023-10-30 18:35:05 +0000

    Create a login functional

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request, flash
-# from flask_login import login_user, LoginManager, current_user, logout_user, login_required
+from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user, login_required
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap5
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -9,17 +9,17 @@ import os
 app = Flask(__name__)
 Bootstrap5(app)
 
-# login_manager = LoginManager()
-# login_manager.init_app(app)
-
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
 db = SQLAlchemy()
 db.init_app(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-class User(db.Model):
+
+class User(UserMixin, db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
@@ -29,6 +29,11 @@ class User(db.Model):
 
 with app.app_context():
     db.create_all()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_or_404(User, user_id)
 
 
 @app.route('/')
@@ -56,6 +61,8 @@ def register():
                 db.session.add(new_user)
                 db.session.commit()
 
+                login_user(new_user)
+
                 return redirect(url_for('home'))
         else:
             flash('The email has already exist. Try to log in')
@@ -66,6 +73,26 @@ def register():
 @app.route('/login', methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
+
+    if request.method == 'POST':
+        if login_form.validate_on_submit():
+            user = User.query.filter_by(email=request.form.get('email')).first()
+
+            if user:
+                user_password = user.password
+                password = login_form.password.data
+                is_password_correct = check_password_hash(user_password, password)
+
+                if is_password_correct:
+                    login_user(user)
+                    return redirect(url_for('home'))
+                else:
+                    flash('Your password is not correct. Try again')
+                    return redirect(url_for('login'))
+            else:
+                flash('The email does not exist. Try again or register')
+                return redirect('login')
+
     return render_template("register.html", form=login_form)
 
 
