da1c845f870a4a03f0eddd08e3b1018ba395b887
Plovester <lovester.solo@gmail.com>
2024-02-18 22:55:19 +0000

    Add new 'deleted_at' column to categories

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
     items = relationship("Item", back_populates="category")
+    deleted_at = db.Column(db.DateTime)
 
 
 class Item(db.Model):
