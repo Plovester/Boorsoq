8e2f3b1cb50d553dd850e9346a48ad507b5513a3
Lyubov P <lyubovp@Lyubovs-Laptop.local>
2025-02-20 14:15:49 +0000

    Add a column 'image_url' for the Category table

--- models.py
+++ models.py
@@ -33,6 +33,7 @@ class Category(db.Model):
     __tablename__ = "categories"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
+    image_url = db.Column(db.String(250), nullable=False)
     items = relationship("Item", back_populates="category")
     deleted_at = db.Column(db.DateTime)
 
