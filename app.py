1fd7be30e19f96564af1cf07be776f602e955e91
Plovester <lovester.solo@gmail.com>
2025-06-05 15:34:07 +0100

    Remove the roles and the first admin creation from the app file

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 import os
 from database import db
-from create_roles import create_roles
-from create_admin import create_admin
 
 migrate = Migrate()
 login_manager = LoginManager()
@@ -24,9 +22,5 @@ def create_app():
 
     db.init_app(app)
     migrate.init_app(app, db)
-
-    with (app.app_context()):
-        create_roles()
-        create_admin()
 
     return app
